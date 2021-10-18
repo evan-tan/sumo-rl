133ad3e8d3f943ce0fd7014aa5986e2d35ca79fc
Evan Tan <tcyevan@gmail.com>
2021-10-18 15:36:41 +0800

    Update custom model

--- custom/model.py
+++ custom/model.py
@@ -1,11 +1,18 @@
+from collections import OrderedDict
+from pathlib import Path
 from typing import Callable, Dict, List, Optional, Tuple, Type, Union
 
 import gym
+import numpy as np
 import torch
+import torch.nn as nn
+import torch.nn.functional as F
 from stable_baselines3 import PPO
 from stable_baselines3.common.policies import ActorCriticPolicy
-from torch import nn
+from torch import nn, optim
+from torch.optim import lr_scheduler
 
+torch.backends.cudnn.benchmark = False
 
 class CustomNetwork(nn.Module):
     """
@@ -13,30 +20,40 @@ class CustomNetwork(nn.Module):
     It receives as input the features extracted by the feature extractor.
 
     :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
-    :param last_layer_dim_pi: (int) number of units for the last layer of the policy network
-    :param last_layer_dim_vf: (int) number of units for the last layer of the value network
+    :param actor_out_dim: (int) number of units for the last layer of the policy network    
+    :param critic_out_dim: (int) number of units for the last layer of the value network
     """
 
     def __init__(
         self,
         feature_dim: int,
-        last_layer_dim_pi: int = 64,
-        last_layer_dim_vf: int = 64,
+        actor_out_dim: int = 64,
+        critic_out_dim: int = 64,
     ):
         super(CustomNetwork, self).__init__()
 
         # IMPORTANT:
         # Save output dimensions, used to create the distributions
-        self.latent_dim_pi = last_layer_dim_pi
-        self.latent_dim_vf = last_layer_dim_vf
+        self.latent_dim_pi = actor_out_dim
+        self.latent_dim_vf = critic_out_dim
 
         # Policy network
         self.policy_net = nn.Sequential(
-            nn.Linear(feature_dim, last_layer_dim_pi), nn.ReLU()
+            nn.Linear(256, actor_out_dim), nn.LeakyReLU()
         )
         # Value network
         self.value_net = nn.Sequential(
-            nn.Linear(feature_dim, last_layer_dim_vf), nn.ReLU()
+            nn.Linear(256, critic_out_dim), nn.LeakyReLU()
+        )
+
+        self._num_layers = 1  # no stacked GRUs
+        self._hidden_size = 256
+        self.gru = nn.GRU(
+            feature_dim,
+            self._hidden_size,
+            self._num_layers,
+            batch_first=True,
+            dropout=0,
         )
 
     def forward(self, features: torch.Tensor) -> Tuple[torch.Tensor, torch.Tensor]:
@@ -44,7 +61,16 @@ class CustomNetwork(nn.Module):
         :return: (torch.Tensor, torch.Tensor) latent_policy, latent_value of the specified network.
             If all layers are shared, then ``latent_policy == latent_value``
         """
-        return self.policy_net(features), self.value_net(features)
+        batch_size = features.size(0)
+        h_init = torch.zeros(self._num_layers, batch_size, self._hidden_size)
+
+        # gru input shape: (batch size, seq_len, num_actions)
+        out, h = self.gru(features, h_init)
+
+        out = nn.LeakyReLU(out[:, -1, :])
+
+        # share input for actor critic
+        return self.policy_net(out), self.value_net(out)
 
 
 class CustomActorCriticPolicy(ActorCriticPolicy):
@@ -73,7 +99,9 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
         self.ortho_init = False
 
     def _build_mlp_extractor(self) -> None:
+        # this is actually a GRU...
         self.mlp_extractor = CustomNetwork(self.features_dim)
+
 
 if __name__ == "__main__":
     model = PPO(CustomActorCriticPolicy, "CartPole-v1", verbose=1)
