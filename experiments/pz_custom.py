4139005ec2360aba970fb0f70a395ffbbb43242e
Evan Tan <tcyevan@gmail.com>
2021-10-18 16:38:00 +0800

    Update with RNN & PPO training

--- experiments/pz_custom.py
+++ experiments/pz_custom.py
@@ -5,6 +5,7 @@ from copy import deepcopy
 
 import numpy as np
 import ray
+import ray.rllib.agents.ppo as ppo
 import sumo_rl
 import supersuit as ss
 import tensorflow as tf
@@ -15,25 +16,80 @@ from custom.model import CustomActorCriticPolicy
 from custom.utils import load_cfg
 from gym.spaces import Box
 from numpy import float32
-from pettingzoo.classic import leduc_holdem_v2
 from ray import tune
 from ray.rllib.agents.registry import get_agent_class
 from ray.rllib.env import PettingZooEnv
 from ray.rllib.models import ModelCatalog
 from ray.rllib.models.modelv2 import ModelV2
+from ray.rllib.models.preprocessors import get_preprocessor
+from ray.rllib.models.tf.recurrent_net import RecurrentNetwork
 from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.models.torch.recurrent_net import RecurrentNetwork as TorchRNN
 from ray.rllib.models.torch.torch_modelv2 import TorchModelV2
+from ray.rllib.utils.annotations import override
 from ray.rllib.utils.framework import try_import_tf, try_import_torch
 from ray.rllib.utils.torch_ops import FLOAT_MAX, FLOAT_MIN
 from ray.tune.registry import register_env
-from stable_baselines3 import PPO
-from stable_baselines3.common.callbacks import EvalCallback
-from stable_baselines3.common.evaluation import evaluate_policy
-from stable_baselines3.common.vec_env import VecMonitor
-from supersuit import dtype_v0
 
-# tf1, tf, tfv = try_import_tf()
-# torch, nn = try_import_torch()
+
+class TorchRNNModel(TorchRNN, nn.Module):
+    def __init__(
+        self,
+        obs_space,
+        action_space,
+        num_outputs,
+        model_config,
+        name,
+        fc_size=64,
+        lstm_state_size=256,
+    ):
+        nn.Module.__init__(self)
+        super().__init__(obs_space, action_space, num_outputs, model_config, name)
+
+        self.obs_size = get_preprocessor(obs_space)(obs_space).size
+        self.fc_size = fc_size
+        self.lstm_state_size = lstm_state_size
+
+        # Build the Module from fc + LSTM + 2xfc (action + value outs).
+        self.fc1 = nn.Linear(self.obs_size, self.fc_size)
+        self.lstm = nn.LSTM(self.fc_size, self.lstm_state_size, batch_first=True)
+        self.action_branch = nn.Linear(self.lstm_state_size, num_outputs)
+        self.value_branch = nn.Linear(self.lstm_state_size, 1)
+        # Holds the current "base" output (before logits layer).
+        self._features = None
+
+    @override(ModelV2)
+    def get_initial_state(self):
+        # TODO: (sven): Get rid of `get_initial_state` once Trajectory
+        #  View API is supported across all of RLlib.
+        # Place hidden states on same device as model.
+        h = [
+            self.fc1.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
+            self.fc1.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
+        ]
+        return h
+
+    @override(ModelV2)
+    def value_function(self):
+        assert self._features is not None, "must call forward() first"
+        return torch.reshape(self.value_branch(self._features), [-1])
+
+    @override(TorchRNN)
+    def forward_rnn(self, inputs, state, seq_lens):
+        """Feeds `inputs` (B x T x ..) through the Gru Unit.
+        Returns the resulting outputs as a sequence (B x T x ...).
+        Values are stored in self._cur_value in simple (B) shape (where B
+        contains both the B and T dims!).
+        Returns:
+            NN Outputs (B x T x ...) as sequence.
+            The state batches as a List of two items (c- and h-states).
+        """
+        x = nn.functional.relu(self.fc1(inputs))
+        self._features, [h, c] = self.lstm(
+            x, [torch.unsqueeze(state[0], 0), torch.unsqueeze(state[1], 0)]
+        )
+        action_out = self.action_branch(self._features)
+        return action_out, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
 
 
 class CustomNetwork(TorchModelV2, torch.nn.Module):
@@ -62,17 +118,10 @@ class CustomNetwork(TorchModelV2, torch.nn.Module):
         self.critic = nn.Linear(512, 1)  # value function
 
     def forward(self, input_dict, state, seq_lens):
-        # Extract the available actions tensor from the observation.
-        action_mask = input_dict["obs"]["action_mask"]
+        # do we need to flatten? if so, use key: 'obs_flat'
+        model_out, self._value_out = self.base_model(input_dict["obs"])
 
-        # Compute the predicted action embedding
-        action_logits, _ = self.action_embed_model(
-            {"obs": input_dict["obs"]["observation"]}
-        )
-        # turns probit action mask into logit action mask
-        inf_mask = torch.clamp(torch.log(action_mask), -1e10, FLOAT_MAX)
-
-        return action_logits + inf_mask, state
+        return model_out, state
 
     def value_function(self):
         return self.action_embed_model.value_function()
@@ -97,7 +146,7 @@ if __name__ == "__main__":
     num_cpus = 6
     num_rollouts = 12
 
-    ModelCatalog.register_custom_model(model_name, CustomNetwork)
+    ModelCatalog.register_custom_model(model_name, TorchRNNModel)
     register_env(env_name, lambda config: PettingZooEnv(env_creator()))
     config = deepcopy(get_agent_class(algo_name)._default_config)
 
@@ -105,11 +154,12 @@ if __name__ == "__main__":
     obs_space = tmp_env.observation_space
     act_space = tmp_env.action_space
 
+    policy_dict = {"policy_0": (None, obs_space, act_space, {})}
+    policy_ids = list(policy_dict.keys())
+    assert len(policy_ids) == 1
     config["multiagent"] = {
-        "policies": {
-            "agent_1": (None, obs_space, act_space, {}),
-        },
-        "policy_mapping_fn": lambda agent_id: agent_id,
+        "policies": policy_dict,
+        "policy_mapping_fn": (lambda agent_id: policy_ids[0]),
     }
 
     config["framework"] = "torch"
@@ -127,7 +177,7 @@ if __name__ == "__main__":
     # config['hiddens'] = []
     config["env"] = env_name
 
-    ray.init(num_cpus=num_cpus + 1)
+    ray.init(num_cpus=num_cpus)
 
     run_name = algo_name + "-" + env_name
     tune.run(
