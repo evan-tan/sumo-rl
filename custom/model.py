ccd95b4d0308b4cfff6a0d3e1f3acb3123c45450
Evan Tan <tcyevan@gmail.com>
2021-10-22 10:59:08 +0800

    Fix missing files

--- custom/model.py
+++ custom/model.py
@@ -14,13 +14,13 @@ from torch.optim import lr_scheduler
 
 torch.backends.cudnn.benchmark = False
 
-class CustomNetwork(nn.Module):
+class CustomNetworkSB3(nn.Module):
     """
     Custom network for policy and value function.
     It receives as input the features extracted by the feature extractor.
 
     :param feature_dim: dimension of the features extracted with the features_extractor (e.g. features from a CNN)
-    :param actor_out_dim: (int) number of units for the last layer of the policy network    
+    :param actor_out_dim: (int) number of units for the last layer of the policy network
     :param critic_out_dim: (int) number of units for the last layer of the value network
     """
 
@@ -30,7 +30,7 @@ class CustomNetwork(nn.Module):
         actor_out_dim: int = 64,
         critic_out_dim: int = 64,
     ):
-        super(CustomNetwork, self).__init__()
+        super(CustomNetworkSB3, self).__init__()
 
         # IMPORTANT:
         # Save output dimensions, used to create the distributions
@@ -100,7 +100,74 @@ class CustomActorCriticPolicy(ActorCriticPolicy):
 
     def _build_mlp_extractor(self) -> None:
         # this is actually a GRU...
-        self.mlp_extractor = CustomNetwork(self.features_dim)
+        self.mlp_extractor = CustomNetworkSB3(self.features_dim)
+
+from ray.rllib.models.modelv2 import ModelV2
+from ray.rllib.models.preprocessors import get_preprocessor
+from ray.rllib.models.torch.fcnet import FullyConnectedNetwork as TorchFC
+from ray.rllib.models.torch.recurrent_net import RecurrentNetwork as TorchRNN
+from ray.rllib.utils.annotations import override
+
+# NOTE: For RLlib ONLY
+class TorchRNNModel(TorchRNN, nn.Module):
+    def __init__(
+        self,
+        obs_space,
+        action_space,
+        num_outputs,
+        model_config,
+        name,
+        fc_size=64,
+        lstm_state_size=256,
+    ):
+        nn.Module.__init__(self)
+        super().__init__(obs_space, action_space, num_outputs, model_config, name)
+
+        self.obs_size = get_preprocessor(obs_space)(obs_space).size
+        self.fc_size = fc_size
+        self.lstm_state_size = lstm_state_size
+
+        # Build the Module from fc + LSTM + 2xfc (action + value outs).
+        self.fc1 = nn.Linear(self.obs_size, self.fc_size)
+        self.lstm = nn.LSTM(self.fc_size, self.lstm_state_size, batch_first=True)
+        self.action_branch = nn.Linear(self.lstm_state_size, num_outputs)
+        self.value_branch = nn.Linear(self.lstm_state_size, 1)
+        # Holds the current "base" output (before logits layer).
+        self._features = None
+
+    @override(ModelV2)
+    def get_initial_state(self):
+        # TODO: (sven): Get rid of `get_initial_state` once Trajectory
+        #  View API is supported across all of RLlib.
+        # Place hidden states on same device as model.
+        h = [
+            self.fc1.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
+            self.fc1.weight.new(1, self.lstm_state_size).zero_().squeeze(0),
+        ]
+        return h
+
+    @override(ModelV2)
+    def value_function(self):
+        assert self._features is not None, "must call forward() first"
+        return torch.reshape(self.value_branch(self._features), [-1])
+
+    @override(TorchRNN)
+    def forward_rnn(self, inputs, state, seq_lens):
+        """Feeds `inputs` (B x T x ..) through the Gru Unit.
+        Returns the resulting outputs as a sequence (B x T x ...).
+        Values are stored in self._cur_value in simple (B) shape (where B
+        contains both the B and T dims!).
+        Returns:
+            NN Outputs (B x T x ...) as sequence.
+            The state batches as a List of two items (c- and h-states).
+        """
+        x = nn.functional.leaky_relu(self.fc1(inputs))
+        self._features, [h, c] = self.lstm(
+            x, [torch.unsqueeze(state[0], 0), torch.unsqueeze(state[1], 0)]
+        )
+        action_out = self.action_branch(self._features)
+        return action_out, [torch.squeeze(h, 0), torch.squeeze(c, 0)]
+
 
 
 if __name__ == "__main__":
--- custom/utils.py
+++ custom/utils.py
@@ -1,6 +1,46 @@
 import json
 
+import numpy as np
+import sumo_rl
 
-def load_cfg(json_file):
-    with open(json_file, 'r') as f:
+
+def load_cfg(json_file) -> np.ndarray:
+    with open(json_file, "r") as f:
         return json.load(f)
+
+
+def unsqueeze(arr: np.ndarray, dim: int) -> np.ndarray:
+    """Wrapper function for torch.unsqueeze() functionality in NumPy"""
+    return np.expand_dims(arr, axis=dim)
+
+
+def env_creator(num_timesteps, parallel=False):
+    """For PettingZoo"""
+
+    env_timestep = 5
+    if parallel:
+        env = sumo_rl.parallel_env(
+            net_file="nets/big-intersection/big-intersection.net.xml",
+            route_file="nets/big-intersection/routes.rou.xml",
+            out_csv_name="outputs/big-intersection/test",
+            use_gui=True,
+            num_seconds=int(num_timesteps),
+            delta_time=env_timestep,
+            yellow_time=2,
+            min_green=5,
+            max_green=50,
+        )
+    else:
+        env = sumo_rl.env(
+            net_file="nets/big-intersection/big-intersection.net.xml",
+            route_file="nets/big-intersection/routes.rou.xml",
+            out_csv_name="outputs/big-intersection/test",
+            use_gui=True,
+            num_seconds=int(num_timesteps),
+            delta_time=env_timestep,
+            yellow_time=2,
+            min_green=5,
+            max_green=50,
+        )
+
+    return env
