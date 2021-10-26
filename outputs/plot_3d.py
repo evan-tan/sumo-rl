970a913bebedccc3136a5a3023c04cb16fde40f6
Christopher Jordan Adis <cadi0002@student.monash.edu>
2021-10-26 20:03:40 +1100

    made into functions, table of summary too

--- outputs/plot_3d.py
+++ outputs/plot_3d.py
@@ -38,71 +38,81 @@ def smooth_data(scalars: List[float], weight: float) -> List[float]:
 
     return smoothed
 
+def plot_info(combined, cat):
+    names = combined[0].columns.tolist()
+    names = [name for name in names if type(name) == str]
+
+
+    fig, axs = plt.subplots(3,4,figsize=(9, 9))
+    axs = np.ndarray.flatten(axs).tolist()
+    fig.delaxes(axs[-1])
+
+    for i, ax in enumerate(axs[:len(names)]):
+        for data in combined:
+            y = data[names[i]].to_numpy()
+            ax.plot(y, alpha=0.1)
+            
+        for data in combined:
+            y = data[names[i]].to_numpy()
+            ax.plot(smooth_data(y, 0.99), alpha=0.8)
+            
+        ax.title.set_text(names[i].replace("_"," ").title())  
+
+    fig.suptitle(cat.title(), fontsize=20) 
+    fig.tight_layout()
+
+    plt.show()
+
+def process_data(folder_path, run_type):
+    files = glob.glob(folder_path + "*")
+    files = pd.DataFrame(files)
+
+    files = pd.concat([files, files[0].str.split("/|\\.|\\_", expand=True).iloc[:,4:(len(files.columns)+4+2)]], axis=1)
+    files = files.sort_values([4, 5, 6], ascending=[False, True, True])
+
+
+
+    files = files[files[4] == run_type]
+
+    dfs = [pd.read_csv(file_) for file_ in files[0].tolist()]
+
+
+    names = dfs[0].dtypes[dfs[0].dtypes == object].index.to_list()
+
+    for i in range(len(dfs)):
+        names = dfs[i].dtypes[dfs[i].dtypes == object].index.to_list()
+        for name in names:
+            dfs[i][name] = dfs[i][name].apply(np.vectorize(str_to_list))
+        
+        dfs[i].name = files.iloc[i,0]
+        dfs[i]["file_name"] = files.iloc[i,0]
+        dfs[i] = pd.merge(dfs[i], files, how="left", left_on=["file_name"], right_on = [0]).drop(["file_name", 0],1)
+
+    test = files.duplicated(subset=[5]).tolist()
+    inds = [i for i, x in enumerate(test) if not x]
+    inds = [(inds[i],inds[i+1]) if i+1 != len(inds) else (inds[i], None) for i in range(len(inds))]
+
+    combine = [dfs[tup[0]:tup[1]] for tup in inds]
+    combined = [pd.concat(comb) for comb in combine]
+    
+    return combined
+
 root_folder = "./outputs/"
 network = "big-intersection/"
 
 folder_path = root_folder + network
 
-files = glob.glob(folder_path + "*")
-
-files = pd.DataFrame(files)
-
-files = pd.concat([files, files[0].str.split("/|\\.|\\_", expand=True).iloc[:,4:(len(files.columns)+4+2)]], axis=1)
-files = files.sort_values([4, 5, 6], ascending=[False, True, True])
-
 run_type = "train"
 
-files = files[files[4] == run_type]
+combined = process_data(folder_path, run_type)
+
+test = pd.concat(combined)
+pair_res = test.groupby([6,5]).agg(['mean']).reset_index()
+run_res = test.groupby([6]).agg(['mean']).reset_index()
 
 
+plot_info(combined, run_type)
 
-
-#files = sorted(files, key=lambda x: int(x.split("run")[-1].replace(".csv","")))
-
-dfs = [pd.read_csv(file_) for file_ in files[0].tolist()]
-
-
-names = dfs[0].dtypes[dfs[0].dtypes == object].index.to_list()
-
-for i in range(len(dfs)):
-    names = dfs[i].dtypes[dfs[i].dtypes == object].index.to_list()
-    for name in names:
-        dfs[i][name] = dfs[i][name].apply(np.vectorize(str_to_list))
-    
-    dfs[i].name = files.iloc[i,0]
-    dfs[i]["file_name"] = files.iloc[i,0]
-    dfs[i] = pd.merge(dfs[i], files, how="left", left_on=["file_name"], right_on = [0]).drop(["file_name", 0],1)
-
-test = files.duplicated(subset=[5]).tolist()
-inds = [i for i, x in enumerate(test) if not x]
-inds = [(inds[i],inds[i+1]) if i+1 != len(inds) else (inds[i], None) for i in range(len(inds))]
-
-combine = [dfs[tup[0]:tup[1]] for tup in inds]
-combined = [pd.concat(comb) for comb in combine]
-
-print("tru")
-
-
-names = combined[0].columns.tolist()
-names = [name for name in names if type(name) == str]
-
-
-fig, axs = plt.subplots(3,4,figsize=(9, 9))
-axs = np.ndarray.flatten(axs).tolist()
-fig.delaxes(axs[-1])
-
-for i, ax in enumerate(axs[:len(names)]):
-    for data in combined:
-        y = data[names[i]].to_numpy()
-        ax.plot(y, alpha=0.1)
-        
-    for data in combined:
-        y = data[names[i]].to_numpy()
-        ax.plot(smooth_data(y, 0.99), alpha=0.8)
-        
-    ax.title.set_text(names[i].replace("_"," ").title())     
-
-plt.show()
 
 # time_steps = dfs[0].iloc[:,0].values
 # time_steps = np.expand_dims(time_steps,0)
